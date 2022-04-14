bc41a664c8c8ff1f4e928c9fa60d168909e6eef9
Kshitij Makkar <k.makkar02@gmail.com>
2022-04-15 04:05:08 +0530

    add firebase and cleanup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCgwiQsYCW2LvmVHzvtUBKt19fnzeV8taE",
+    authDomain: "e-com-29841.firebaseapp.com",
+    projectId: "e-com-29841",
+    storageBucket: "e-com-29841.appspot.com",
+    messagingSenderId: "869487068173",
+    appId: "1:869487068173:web:3bb8c3e3694122e4d58144",
+    measurementId: "G-YYZBBL16KM"
+  };

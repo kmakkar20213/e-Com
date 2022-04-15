0d0e0ae40d19adc555e6a30a912ccd5a6b497afd
kshitij20213 <kshitij20213@iiitd.ac.in>
2022-04-16 04:29:53 +0530

    header complete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from "./Header";
 function App() {
     return (
         <div className="App">
-            <h1>Hello people</h1>
             <Header />
         </div>
     );
--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 function Header() {
     return (
         <div className="header">
-            <img className="header_logo" src="" />
+            <img
+                className="header_logo"
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            />
 
             <div className="header_search">
                 <input className="header_ser" type="text" />
+                <SearchIcon className="ser_icon" />
             </div>
 
             <div className="header_nav">
                 <div className="header_option">
-                    <span className="option_one">Hello People</span>
+                    <span className="option_one">Hello,</span>
                     <span className="option_two">Sign In</span>
                 </div>
                 <div className="header_option">
@@ -23,10 +29,11 @@ function Header() {
                     <span className="option_one">Your</span>
                     <span className="option_two">Prime</span>
                 </div>
-                <div className="header_option">
-                    <span className="option_one">Hello People</span>
-                    <span className="option_two">Sign In</span>
-                </div>
+            </div>
+
+            <div className="basket">
+                <ShoppingBasketIcon className="bask_icon" />
+                <span className="option_two">0</span>
             </div>
         </div>
     );

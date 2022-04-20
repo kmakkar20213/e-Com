159e198e3fa15de52b83d479c1adccc0d20f8aef
kshitij20213 <kshitij20213@iiitd.ac.in>
2022-04-21 02:46:39 +0530

    prods

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <Home />
-        </div>
+        <Router>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
--- src/Product.js
+++ src/Product.js
@@ -5,10 +5,12 @@ function Product(props) {
     return (
         <div className="product_card">
             <img className="product_img" src={props.img} />
-            <h3 className="product_title">{props.title}</h3>
-            <h4 className="product_price">{props.price}</h4>
-            <p className="rating">{props.rating}</p>
-            <button className="cart_button">Add To Cart</button>
+            <div className="product_info">
+                <h3 className="product_title">{props.title}</h3>
+                <h4 className="product_price">{props.price}</h4>
+                <p className="rating">{props.rating}</p>
+                <button className="cart_button">Add To Cart</button>
+            </div>
         </div>
     );
 }

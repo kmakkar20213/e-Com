ec1327afcd34e9ebc49ee8cd2f1815dd5b7e8c96
kshitij20213 <kshitij20213@iiitd.ac.in>
2022-04-24 16:04:48 +0530

    create Orders page

--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateVariable } from "./StateProvider";
+import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import Orders from "./Orders";
+
+const promise = loadStripe(
+    "pk_test_51KrUQeSCwLb7eGXTOxqLz0U5sEEq8i9jOJIBfTIO6EUrEiEHyEhIsmOqIeusvjWoM4sQIt3A8U0rFhZDSQGFmRDL00Ii6u2xKG"
+);
 
 function App() {
     const [{}, dispatch] = useStateVariable();
 
     useEffect(() => {
         auth.onAuthStateChanged((authUser) => {
-            console.log(authUser);
-
             if (authUser) {
                 dispatch({
                     type: "SET_USER",
@@ -35,6 +41,16 @@ function App() {
                     <Route exact path="/" element={<Home />} />
                     <Route exact path="/checkout" element={<Checkout />} />
                     <Route exact path="/login" element={<Login />} />
+                    <Route
+                        exact
+                        path="/payment"
+                        element={
+                            <Elements stripe={promise}>
+                                <Payment />
+                            </Elements>
+                        }
+                    />
+                    <Route exact path="/orders" element={<Orders />} />
                 </Routes>
             </div>
         </Router>
